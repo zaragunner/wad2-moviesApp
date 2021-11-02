fd5db01e4aeb24bb134b7e046137073c0a8fb62c
Zara Gunner <20084586@mail.wit.ie>
2021-11-02 10:56:34 +0000

    E2E tests - Home page - Filtering by genre.

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -64,7 +64,21 @@ describe("Home Page ", () => {
            
           })
        })
-       describe("By movie genre" ,() => {
+       describe("By movie genre", () => {
+        it("should display movies with the specified genre only", () => {
+           const selectedGenreId = 35;
+           const selectedGenreText = "Comedy";
+           const matchingMovies = filterByGenre(movies, selectedGenreId);
+           cy.get("#genre-select").click();
+           cy.get("li").contains(selectedGenreText).click();
+           cy.get(".MuiCardHeader-content").should(
+             "have.length",
+             matchingMovies.length
+           );
+           cy.get(".MuiCardHeader-content").each(($card, index) => {
+             cy.wrap($card).find("p").contains(matchingMovies[index].title);
+           });
+         });
        });
-    });
-    });
+     });
+   });
