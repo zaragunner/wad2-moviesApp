e4dceebe5616d6b97c84fe2ebbb1e1ceaaa09238
Zara Gunner <20084586@mail.wit.ie>
2021-11-08 12:11:35 +0000

    Add new review support.

--- src/contexts/moviesContext.js
+++ src/contexts/moviesContext.js
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 export const MoviesContext = React.createContext(null);
 
 const MoviesContextProvider = (props) => {
+    const [myReviews, setMyReviews] = useState( {} ) 
   const [favorites, setFavorites] = useState( [] )
 
   const addToFavorites = (movie) => {
@@ -15,13 +16,17 @@ const MoviesContextProvider = (props) => {
       (mId) => mId !== movie.id
     ) )
   };
+  const addReview = (movie, review) => {
+    setMyReviews( {...myReviews, [movie.id]: review } )
+  };
 
   return (
     <MoviesContext.Provider
       value={{
         favorites,
         addToFavorites,
-        removeFromFavorites
+        removeFromFavorites,
+        addReview,
       }}
     >
       {props.children}
