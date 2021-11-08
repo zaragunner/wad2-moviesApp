b396d2af02282be03082577f2e3831cd544357fb
Zara Gunner <20084586@mail.wit.ie>
2021-11-08 11:57:11 +0000

    Bug Fix - make action icons on movie card configurable.

--- src/components/movieCard/index.js
+++ src/components/movieCard/index.js
@@ -25,7 +25,7 @@ const useStyles = makeStyles({
   },
 });
 
-export default function MovieCard({ movie }) {
+export default function MovieCard({ movie, action }) {
   const classes = useStyles();
   const { favorites, addToFavorites } = useContext(MoviesContext);
 
@@ -81,9 +81,7 @@ export default function MovieCard({ movie }) {
         </Grid>
       </CardContent>
       <CardActions disableSpacing>
-      <IconButton aria-label="add to favorites" onClick={handleAddToFavorite}>
-        <FavoriteIcon color="primary" fontSize="large" />
-    </IconButton>
+      {action(movie)}
         <Link to={`/movies/${movie.id}`}>
         <Button variant="outlined" size="medium" color="primary">
           More Info ...
--- src/pages/favoriteMoviesPage.js
+++ src/pages/favoriteMoviesPage.js
@@ -3,7 +3,9 @@ import PageTemplate from "../components/templateMovieListPage";
 import { MoviesContext } from "../contexts/moviesContext";
 import { useQueries } from "react-query";
 import { getMovie } from "../api/tmdb-api";
-import Spinner from '../components/spinner'
+import Spinner from '../components/spinner';
+import RemoveFromFavorites from "../components/cardIcons/removeFromFavorites";
+import WriteReview from "../components/cardIcons/writeReview";
 
 const FavoriteMoviesPage = () => {
   const {favorites: movieIds } = useContext(MoviesContext);
@@ -25,14 +27,20 @@ const FavoriteMoviesPage = () => {
   }
   const movies = favoriteMovieQueries.map((q) => q.data);
   const toDo = () => true;
-
   return (
     <PageTemplate
-      title="Favourite Movies"
+      title="Favorite Movies"
       movies={movies}
-      selectFavorite={toDo}
-    />
-  );
+      action={(movie) => {
+        return (
+          <>
+            <RemoveFromFavorites movie={movie} />
+            <WriteReview movie={movie} />
+          </>
+   );
+  }}
+/>
+);
 };
 
 export default FavoriteMoviesPage;
