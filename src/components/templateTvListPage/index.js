a1d19fcf01f1fc0af7ddcf1b6efa12dbc26fed34
Zara Gunner <20084586@mail.wit.ie>
2021-12-02 22:30:31 +0000

    Added filter and search functionality

--- src/components/templateTvListPage/index.js
+++ src/components/templateTvListPage/index.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 // import Header from "../headerMovieList";
-// import FilterCard from "../filterMoviesCard";
+import FilterCard from "../filterTvCard";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import TvList from "../tvList";
@@ -13,31 +13,38 @@ const useStyles = makeStyles({
 
 function TvListPageTemplate({ shows, action }) {
   const classes = useStyles();
-  // const [nameFilter, setNameFilter] = useState("");
-  // const [genreFilter, setGenreFilter] = useState("0");
-  // const genreId = Number(genreFilter);
+  const [nameFilter, setNameFilter] = useState("");
+  const [genreFilter, setGenreFilter] = useState("0");
+  const genreId = Number(genreFilter);
 
-  // let displayedShows = shows
-  //   .filter((tv) => {
-  //     return tv.name.toLowerCase().search(nameFilter.toLowerCase()) !== -1;
-  //   })
-  //   .filter((tv) => {
-  //     return genreId > 0 ? tv.genre_ids.includes(genreId) : true;
-  //   });
+  let displayedShows = shows
+    .filter((tv) => {
+      return tv.name.toLowerCase().search(nameFilter.toLowerCase()) !== -1;
+    })
+    .filter((tv) => {
+      return genreId > 0 ? tv.genre_ids.includes(genreId) : true;
+    });
 
-  // const handleChange = (type, value) => {
-  //   if (type === "name") setNameFilter(value);
-  //   else setGenreFilter(value);
-  // };
+  const handleChange = (type, value) => {
+    if (type === "name") setNameFilter(value);
+    else setGenreFilter(value);
+  };
 
   return (
     <Grid container className={classes.root}>
-      <Grid item xs={12}>
-        {/* <Header title={title} /> */}
+       <Grid item xs={12}>
+        
       </Grid>
       <Grid item container spacing={5}>
-       
-        <TvList action={action} shows={shows}></TvList> </Grid>
+      <Grid key="find" item xs={12} sm={6} md={4} lg={3} xl={2}>
+          <FilterCard
+            onUserInput={handleChange}
+            titleFilter={nameFilter}
+            genreFilter={genreFilter}
+          />
+        </Grid>
+        
+        <TvList action={action} shows={displayedShows}></TvList> </Grid>
     </Grid>
   );
 }
