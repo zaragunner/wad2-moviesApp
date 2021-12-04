b2dd80a4ea18023f622fe3d642d30aad68e40a53
Zara Gunner <20084586@mail.wit.ie>
2021-12-04 21:32:12 +0000

    Added top rated page

---
+++ src/pages/topRatedPage.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PageTemplate from "../components/templateMovieListPage";
+import { useQuery } from 'react-query'
+import Spinner from '../components/spinner'
+import {getTopRated} from '../api/tmdb-api'
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+
+const TopRatedPage = (props) => {
+  const {  data, error, isLoading, isError }  = useQuery('top rated', getTopRated)
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const movies = data.results;
+
+  // Redundant, but necessary to avoid app crashing.
+  const favorites = movies.filter(m => m.favorite)
+  localStorage.setItem('favorites', JSON.stringify(favorites))
+ 
+
+  return (
+    <PageTemplate
+      title="Top Rated Movies"
+      movies={movies}
+      action={(movie) => {
+        return <AddToFavoritesIcon movie={movie} />
+      }}
+    />
+  );
+};
+
+export default TopRatedPage;
