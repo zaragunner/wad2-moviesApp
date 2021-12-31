16e26f21ed5cb9b8eafddb0e7ef72dca0e5751ce
Zara Gunner <20084586@mail.wit.ie>
2021-12-31 22:40:31 +0000

    Added back in some movie details i had removed while testing

--- src/components/movieDetails/index.js
+++ src/components/movieDetails/index.js
@@ -1,5 +1,11 @@
 import React, { useState} from "react";
-
+import Chip from "@material-ui/core/Chip";
+import Paper from "@material-ui/core/Paper";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import MonetizationIcon from "@material-ui/icons/MonetizationOn";
+import StarRate from "@material-ui/icons/StarRate";
+import NavigationIcon from "@material-ui/icons/Navigation";
+import Fab from "@material-ui/core/Fab";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
@@ -39,7 +45,49 @@ const MovieDetails = ({ movie }) => {  // Don't miss this!
         {movie.overview}
       </Typography>
 
-     
+      <Paper component="ul" className={classes.root}>
+        <li>
+          <Chip label="Genres" className={classes.chip} color="primary" />
+        </li>
+        {movie.genres.map((g) => (
+          <li key={g.name}>
+            <Chip label={g.name} className={classes.chip} />
+          </li>
+        ))}
+      </Paper>
+      <Paper component="ul" className={classes.root}>
+        <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
+        <Chip
+          icon={<MonetizationIcon />}
+          label={`${movie.revenue.toLocaleString()}`}
+        />
+        <Chip
+          icon={<StarRate />}
+          label={`${movie.vote_average} (${movie.vote_count})`}
+        />
+        <Chip label={`Released: ${movie.release_date}`} />
+      </Paper>
+
+      <Paper component="ul" className={classes.root}>
+        <li>
+          <Chip label="Production Countries" className={classes.chip} color="primary" />
+        </li>
+        {movie.production_countries.map((g) => (
+          <li key={g.name}>
+            <Chip label={g.name} className={classes.chip} />
+          </li>
+        ))}
+      </Paper>
+
+      <Fab
+        color="secondary"
+        variant="extended"
+        onClick={() =>setDrawerOpen(true)}
+        className={classes.fab}
+      >
+        <NavigationIcon />
+        Reviews
+      </Fab>
       <Drawer anchor="top" open={drawerOpen} onClose={() => setDrawerOpen(false)}>
         <MovieReviews movie={movie} />
       </Drawer>
