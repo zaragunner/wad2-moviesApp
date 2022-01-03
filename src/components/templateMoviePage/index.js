bc7d49267981c61652567faadedfca45d389726b
Zara Gunner <20084586@mail.wit.ie>
2022-01-03 16:15:15 +0000

    finished swaggger docs, fixed movie images loading in on movieDetailsPage

--- src/components/templateMoviePage/index.js
+++ src/components/templateMoviePage/index.js
@@ -3,10 +3,10 @@ import MovieHeader from "../headerMovie";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import GridList from "@material-ui/core/ImageList";
-// import GridListTile from "@material-ui/core/ImageListItem";
-// import { getMovieImages } from "../../api/tmdb-api";
-// import { useQuery } from "react-query";
-// import Spinner from '../spinner'
+import GridListTile from "@material-ui/core/ImageListItem";
+import { getMovieImages } from "../../api/tmdb-api";
+import { useQuery } from "react-query";
+import Spinner from '../spinner'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,7 +22,19 @@ const useStyles = makeStyles((theme) => ({
 
 const TemplateMoviePage = ({ movie, children }) => {
   const classes = useStyles();
-  // 
+  const { data , error, isLoading, isError } = useQuery(
+    ["images", { id: movie.id }],
+    getMovieImages
+  );
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+  const images = data.posters 
 
   return (
     <>
@@ -32,7 +44,14 @@ const TemplateMoviePage = ({ movie, children }) => {
         <Grid item xs={3}>
           <div className={classes.root}>
             <GridList rowHeight={500} className={classes.gridList} cols={1}>
-             
+              {images.map((image) => (
+                <GridListTile key={image.file_path} className={classes.gridListTile} cols={1}>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
+                    alt={image.poster_path}
+                  />
+                </GridListTile>
+              ))}
             </GridList>
           </div>
         </Grid>
@@ -44,5 +63,4 @@ const TemplateMoviePage = ({ movie, children }) => {
     </>
   );
 };
-
 export default TemplateMoviePage;
